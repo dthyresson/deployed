ef855a760046b9a3ba98c2fc4dfb3c57b3a11f62
A. David Thyresson <david@sportsilab.com>
2020-07-12 14:40:20 -0400

    Try setting dotenv in Prisma db

--- api/src/lib/db.js
+++ api/src/lib/db.js
@@ -1,6 +1,9 @@
 // See https://www.prisma.io/docs/reference/tools-and-interfaces/prisma-client/constructor
 // for options.
 
+import * as dotenv from 'dotenv'
 import { PrismaClient } from '@prisma/client'
+
+dotenv.config()
 
 export const db = new PrismaClient()
