c6802f94bb1a7cf856d1b3318867feb5630cace8
A. David Thyresson <david@sportsilab.com>
2020-07-16 13:57:25 -0400

    Prep for jwt payload verification

--- api/src/functions/deploy.js
+++ api/src/functions/deploy.js
@@ -1,7 +1,14 @@
+const jwt = require('jsonwebtoken')
 export const handler = async (event, _context) => {
-  console.log(event.body)
+  const body = JSON.parse(event.body)
+  const decoded = jwt.decode(body.payload)
+  const verified = jwt.verify(body.payload, 'shhhhh')
+  // const notverified = jwt.verify(body.payload, 'foo')
+  console.log(decoded)
+  console.log(verified)
+  // console.log(notverified)
   return {
     statusCode: 200,
-    body: JSON.stringify({ data: event.body }),
+    body: JSON.stringify({ data: decoded }),
   }
 }
